39bd432853c3e7fa62e40904c130777fdbbac1e1
Elena Camacho Aguilar <ecamacho.90@gmail.com>
2023-02-04 15:16:05 -0600

    Added citation details to Readme

--- FixedData/Readme.m
+++ FixedData/Readme.m
@@ -20,7 +20,10 @@
 % 5.- (OPTIONAL) After running Master_1_ImageProcessing you can use
 % Master_2_DataAnalysis to keep better track of the plots that you make
 
-% 6.- If you use this code, please give credit! Thanks
+% 6.- If you use this code, please give credit citing:
+Elena Camacho Aguilar. (2022). ecamacho90/BiologicalImageProcessing: BiologicalImageProcessing pre-release v0.0.1 (v0.0.1). Zenodo. https://doi.org/10.5281/zenodo.6626223
+
+Thanks!
 
 %%
 % Elena Camacho-Aguilar, 2022
